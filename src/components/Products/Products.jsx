a5fe49c98298caed92fbb99194605e79424fe809
Ellis <ellischung98@gmail.com>
2022-04-15 00:38:17 -0400

    added empty products/product

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Grid from "@mui/material";
+
+const products = [
+  {
+    id: 1,
+    name: "GPU",
+    description: "Nvidia RTX 3080 Founders Edition",
+    price: "$1000",
+  },
+  {
+    id: 2,
+    name: "CPU",
+    description: "Intel Core i9-12900k",
+    price: "$500",
+  },
+];
+
+const Products = () => {
+  <main>
+    <Grid container justify="center" spacing={4}>
+      {products.map((product) => (
+        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+          <Product product={product} />
+        </Grid>
+      ))}
+    </Grid>
+  </main>;
+};
+
+export default Products;
