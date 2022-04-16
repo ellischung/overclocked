e2866cbd447f53b37a0ecd2e22899fed2d25f88b
Ellis <ellischung98@gmail.com>
2022-04-16 02:36:10 -0400

    navbar completed

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import Product from "./Product/Product";
+import useStyles from "./styles";
 
 const products = [
   {
@@ -8,7 +9,8 @@ const products = [
     name: "GPU",
     description: "Nvidia RTX 3080 Founders Edition",
     price: "$1000",
-    image: "https://c1.neweggimages.com/ProductImageCompressAll1280/ARUXS211015278F9.jpg",
+    image:
+      "https://c1.neweggimages.com/ProductImageCompressAll1280/ARUXS211015278F9.jpg",
   },
   {
     id: 2,
@@ -20,8 +22,11 @@ const products = [
 ];
 
 const Products = () => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
