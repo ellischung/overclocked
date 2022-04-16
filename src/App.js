665ab07760df34605b239ecd0b3c41de8f6d4564
Ellis <ellischung98@gmail.com>
2022-04-16 02:27:02 -0400

    fixed theming in navbar styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Navbar, Products } from "./components";
 
 const App = () => {
+  const theme = createTheme(); // components need to be wrapped by theme
+
   return (
     <div>
-      <Navbar />
-      <Products />
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Products />
+      </ThemeProvider>
     </div>
   );
 };
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,9 +1,15 @@
 import { makeStyles } from "@mui/styles";
 
-export default makeStyles(() => ({
+const drawerWidth = 0;
+
+export default makeStyles((theme) => ({
   appBar: {
     boxShadow: "none",
     borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
   },
   title: {
     flexGrow: 1,
@@ -15,20 +21,30 @@ export default makeStyles(() => ({
     marginRight: "10px",
   },
   menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
   },
   grow: {
     flexGrow: 1,
   },
   search: {
     position: "relative",
+    borderRadius: theme.shape.borderRadius,
     backgroundColor: "white",
     "&:hover": {
       backgroundColor: "grey",
     },
+    marginRight: theme.spacing(2),
     marginLeft: 0,
     width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "auto",
+    },
   },
   searchIcon: {
+    padding: theme.spacing(0, 2),
     height: "100%",
     position: "absolute",
     pointerEvents: "none",
@@ -40,7 +56,13 @@ export default makeStyles(() => ({
     color: "inherit",
   },
   inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
     width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
   },
 }));
