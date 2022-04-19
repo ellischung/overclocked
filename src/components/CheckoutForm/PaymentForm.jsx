9247fcef0a8b2dd0baa5270f6eed0cb53f790444
Ellis <ellischung98@gmail.com>
2022-04-19 19:25:36 -0400

    finished layout for order review

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Typography, Button, Divider } from "@mui/material";
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Review from "./Review";
 
-const PaymentForm = () => {
-    return (
-        <div>
-            Payment Form
-        </div>
-    );
+const PaymentForm = ({ checkoutToken }) => {
+  return (
+    <>
+      <Review checkoutToken={checkoutToken} />
+    </>
+  );
 };
 
 export default PaymentForm;
