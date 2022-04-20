37b7469656279495bfc7f2745e5df9d7894de987
Ellis <ellischung98@gmail.com>
2022-04-19 20:04:11 -0400

    stripe added to app payment form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const App = () => {
   const theme = createTheme(); // components need to be wrapped by theme
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -36,6 +38,24 @@ const App = () => {
   const handleEmptyCart = async () => {
     const { cart } = await commerce.cart.empty();
     setCart(cart);
+  };
+
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(
+        checkoutTokenId,
+        newOrder
+      );
+      setOrder(incomingOrder);
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
   };
 
   useEffect(() => {
@@ -68,7 +88,18 @@ const App = () => {
                 />
               }
             />
-            <Route exact path="/checkout" element={<Checkout cart={cart} />} />
+            <Route
+              exact
+              path="/checkout"
+              element={
+                <Checkout
+                  cart={cart}
+                  order={order}
+                  onCaptureCheckout={handleCaptureCheckout}
+                  error={errorMessage}
+                />
+              }
+            />
           </Routes>
         </ThemeProvider>
       </div>
